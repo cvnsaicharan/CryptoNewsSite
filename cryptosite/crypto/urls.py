f21e2a4c3822d397ea652d038832789abde53074
cvnsaicharan <43728592+cvnsaicharan@users.noreply.github.com>
2019-07-16 10:56:18 +0530

    Update urls.py

--- cryptosite/crypto/urls.py
+++ cryptosite/crypto/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('signup/', views.signup, name="signup"),
     path('prices/', views.prices, name="prices"),
     path('news/', views.news, name="news"),
